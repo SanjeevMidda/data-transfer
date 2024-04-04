a25383aa53c15103bc751d5584df68e14a9f6f95
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2024-04-04 21:56:13 +0100

    Add files via upload

---
+++ backend/api/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Data received!");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("HELLO!");
+});
+
+app.listen(port, () => {
+  console.log(`Server up and running on port: ${port}`);
+});
